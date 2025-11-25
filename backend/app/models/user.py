cb01a1b03a99ff5c362050647a13be8f36bab40a
Nahoov <newnahomi15@gmail.com>
2025-11-25 00:54:18 -0300

    build: model user &  squemas

---
+++ backend/app/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Date
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import CITEXT
+from app.database import Base # onde Base = declarative_base()
+
+
+
+
+class Usuario(Base):
+    # Modelo do Usuario
+
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String(45), nullable=False)
+    #sobrenome = Column(String(60), nullable=False)
+    email = Column(CITEXT(100), nullable=False, unique=True)
+    senha_hash = Column(String(255), nullable=False)
+    tipo_usuario =  Column(String(20), nullable=False, default="user")
+    data_nascimento = Column(Date, nullable=False)
+    data_cadastro = Column(DateTime, nullable=False, server_default=func.now())
+    ultimo_login = Column(DateTime, nullable=True)
+    
