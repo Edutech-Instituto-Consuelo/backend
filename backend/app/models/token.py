f4477b346653a194524050e49fa50b7727562480
Gabrielly <oliveiragabrielly.dev@gmail.com>
2025-11-26 00:08:27 -0300

    Tabela para armazenar tokens

---
+++ backend/app/models/token.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class RefreshToken(Base):
+    #Tabela para guardar os tokens de refresh
+
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, unique=True, index=True)
+    expires_at = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    revoked_at = Column(DateTime, nullable=True)
+    
+    usuario_id = Column(Integer, ForeignKey("usuarios.id")) # Link com a tabela Usuarios
+    
+    # Aqui precisamos usar string "User" para evitar erro de importação se o User não estiver aqui
+    usuario = relationship("Usuario", back_populates="refresh_tokens")
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -19,3 +19,6 @@ class Usuario(Base):
     data_nascimento = Column(Date, nullable=False)
     data_cadastro = Column(DateTime, nullable=False, server_default=func.now())
     ultimo_login = Column(DateTime, nullable=True)
+    
+    #Caso queira deletar um usuario, todos os tokens de refresh associados a ele também serão deletados
+    refresh_tokens = relationship("RefreshToken", back_populates="usuario", cascade="all, delete-orphan")
