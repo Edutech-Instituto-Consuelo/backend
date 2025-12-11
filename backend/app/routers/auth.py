1dc80b10fddc9f6a63f24c529faedc0d747793fe
labarros <0xlarissa.bf@gmail.com>
2025-12-11 18:35:55 -0300

    feat: :sparkles: Adiciona rota e schema de recuperação de senha do usuario, rota de confirmação de troca de senha e um get de renovação de token

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -85,3 +85,55 @@ def get_me(db:Session = Depends(get_db), usuario = Depends(allowed_roles())):
 	"""Rota para obter informações do usuário autenticado"""
 	user = db.query(Usuario).filter(Usuario.id == usuario["id"]).first()
 	return user
+
+
+# Rota para solicitar redefinicao de senha.
+#recebe o email, verifica se existe e gera um token que sera enviado por email.
+@router.post("/recover", status_code=status.HTTP_200_OK)
+def recuperar_senha(
+    dados: Recuperacaodesenha, 
+    db: Session = Depends(get_db)
+):
+    user = db.query(Usuario).filter(Usuario.email == dados.email).first()
+
+    if user:
+        print(f"DEBUG: Simulação de envio de email para {user.email}")
+        pass
+    return {"msg": "Você receberá um link de recuperação via email."}
+
+
+# Rota POST: confirmar redefinicao de senha.
+# Recebe o token (vindo do e-mail) e a nova senha, valida e atualiza no banco.
+@router.post("/recover/confirm", status_code=status.HTTP_200_OK)
+def confirmar_recuperacao(
+    dados: Novasenha, 
+    db: Session = Depends(get_db)
+):  
+
+    # Busca usuário no banco
+    user = db.query(Usuario).filter(Usuario.email == email_usuario_simulado).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    # Cria nova hash de senha
+    senha_com_salt = create_salt(dados.nova_senha, user.email)
+    novo_hash = get_password_hash(senha_com_salt)
+
+    # 4. Atualizar e Salvar
+    user.senha_hash = novo_hash
+    db.commit()
+
+    return {"msg": "Senha alterada com sucesso!"}
+
+
+# Rota renova token de autentificacao sem a necessidade de fazer login novamente.
+@router.get("/refresh", response_model=TokenResponse)
+def refresh_token(
+    usuario_atual = Depends(allowed_roles()) 
+):
+    	novo_token = create_access_token(
+        user_id=usuario_atual["id"], 
+        email=usuario_atual["email"], 
+        role=usuario_atual["role"] # ou 'tipo_usuario' dependendo de como retorna
+    )
+	return {"access_token": novo_token}
